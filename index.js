85a74bd1b112409b6781e950f0648eb26ced662c
aviadamar <avia2154@gmail.com>
2021-08-05 15:51:37 +0300

    git commit -m 'new commit'

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const { ApolloServer } = require('apollo-server');
+const { ApolloServer, PubSub } = require('apollo-server');
 const mongoose = require('mongoose');
 
 const typeDefs = require('./graphql/typeDefs');
 const resolvers = require('./graphql/resolvers');
 const { MONGODB } = require('./config');
 
-
-
+const pubsub = new PubSub();
 
 const server = new ApolloServer({
     typeDefs,
-    resolvers
+    resolvers,
+    context: ({ req }) => ({ req, pubsub })
 });
 
 mongoose.connect( MONGODB, { useNewUrlParser: true } )
@@ -22,5 +22,3 @@ mongoose.connect( MONGODB, { useNewUrlParser: true } )
         console.log(`Server running ${res.url}`);
     })
 
-
-   
